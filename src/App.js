a0e6839db688e9bc60c6ebca18d214ec8d4e8400
Ryu0012 <seungjy10@gmail.com>
2024-03-08 16:00:31 +0900

    feat(TodoList): Create new project

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
-import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
-import styles from "./App.module.css";
-
-function Hello({ title }) {
-  const [text, setText] = useState("");
-
-  useEffect(() => {
-    console.log("I run when 'keyword' change");
-  }, [text]);
-
-  return (
-    <div>
-      <h1 className={styles.title}>{title}</h1>
-      <input
-        type="text"
-        placeholder="keyword"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      ></input>
-    </div>
-  );
-}
-
-Hello.propTypes = {
-  title: PropTypes.string.isRequired,
-};
+import { useState } from "react";
 
 function App() {
-  const [btn, setBtn] = useState(false);
-  const onClick = () => setBtn(!btn);
-
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodos((currentTodo) => [[todo, false], ...currentTodo]);
+    setTodo("");
+  };
+  const onDeleteList = (index) => {
+    const updateTodos = todos.filter((_, i) => i !== index);
+    setTodos(updateTodos);
+  };
+  const onCheck = (index) => {
+    const todoList = [...todos];
+    todoList[index][1] = !todoList[index][1];
+    setTodos(todoList);
+  };
   return (
     <div>
-      {btn ? null : <Hello title={"Hello world"} />}
-      <button onClick={onClick}>{btn ? "Open" : "Hide"}</button>
+      <h1>ToDo List</h1>
+      <form>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Write to do..."
+          value={todo}
+        ></input>
+        <button onClick={onSubmit}>Btn</button>
+      </form>
+      <hr></hr>
+      <ul>
+        {todos.map((item, index) => (
+          <li
+            key={index}
+            style={
+              item[1]
+                ? { textDecoration: "line-through" }
+                : { textDecoration: "none" }
+            }
+          >
+            {item}
+            <input type="checkbox" onClick={() => onCheck(index)}></input>
+            <button onClick={() => onDeleteList(index)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

9b6b50e7aef0ef6dcf95d74b4e9040f1e088ca23
Ryu0012 <seungjy10@gmail.com>
2024-03-12 18:21:17 +0900

    feat(Movie App): Create new project

--- src/App.js
+++ src/App.js
@@ -1,99 +1,42 @@
-import { useState } from "react";
-
-function MinutesToHours() {
-  const [amount, setAmount] = useState();
-  const [inverted, setInverted] = useState(false);
-  const onChange = (event) => {
-    setAmount(event.target.value);
-  };
-  const reset = () => setAmount(0);
-  const onInvert = () => {
-    setInverted((current) => !current);
-    reset();
-  };
-  return (
-    <div>
-      <div>
-        <h3>Minu 2 H</h3>
-        <label htmlFor="minutes">Minutes</label>
-        <input
-          id="minutes"
-          placeholder="Minutes"
-          value={inverted ? amount * 60 : amount}
-          type="number"
-          onChange={onChange}
-          disabled={inverted}
-        />
-        <label htmlFor="hours">Hours</label>
-        <input
-          id="hours"
-          placeholder="Hours"
-          value={inverted ? amount : Math.round(amount / 60)}
-          type="number"
-          onChange={onChange}
-          disabled={!inverted}
-        />
-      </div>
-      <button onClick={reset}>Reset</button>
-      <button onClick={onInvert}>{inverted ? "Turn back" : "Invert"}</button>
-    </div>
-  );
-}
-
-function KmToMiles() {
-  const [amount, setAmount] = useState();
-  const [inverted, setInverted] = useState(false);
-  const onChange = (event) => setAmount(event.target.value);
-  const reset = () => setAmount(0);
-  const onInvert = () => {
-    setInverted((current) => !current);
-    reset();
-  };
-  return (
-    <div>
-      <h3>Km 2 M</h3>
-      <div>
-        <label htmlFor="km">Km</label>
-        <input
-          id="km"
-          type="number"
-          placeholder="Km"
-          value={inverted ? amount * 1.6 : amount}
-          onChange={onChange}
-          disabled={inverted}
-        ></input>
-        <label htmlFor="mile">Mile</label>
-        <input
-          id="mile"
-          type="number"
-          placeholder="Mile"
-          value={inverted ? amount : amount * 0.621}
-          disabled={!inverted}
-          onChange={onChange}
-        ></input>
-      </div>
-      <button onClick={reset}>reset</button>
-      <button onClick={onInvert}>{inverted ? "Turn back" : "Invert"}</button>
-    </div>
-  );
-}
+import { useEffect, useState } from "react";
 
 function App() {
-  const [index, setIndex] = useState("xx");
-  const onSelect = (event) => {
-    setIndex(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const data = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+    );
+    const json = await data.json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div>
-      <h1>Super Converter </h1>
-      <select onChange={onSelect} value="xx">
-        <option value="xx">Select your units</option>
-        <option value="0">Minutes & Hours</option>
-        <option value="1">Km & Miles</option>
-      </select>
-      {index === "0" ? <MinutesToHours /> : null}
-      {index === "1" ? <KmToMiles /> : null}
-    </div>
+    <h1>
+      {loading ? (
+        "Loding ..."
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.key}>
+              <img src={movie.medium_cover_image} alt={movie.title}></img>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </h1>
   );
 }
 

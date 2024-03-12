0e38cea461861797711f95b9c28ca5ecf01d20c2
Ryu0012 <seungjy10@gmail.com>
2024-03-12 18:31:54 +0900

    chore(*): Reorganize project directory structure

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
-import { useEffect, useState } from "react";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const data = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
-    );
-    const json = await data.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return (
-    <h1>
-      {loading ? (
-        "Loding ..."
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.key}>
-              <img src={movie.medium_cover_image} alt={movie.title}></img>
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </h1>
-  );
+  return <Home />;
 }
 
 export default App;
